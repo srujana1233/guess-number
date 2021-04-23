9508ff9686217a43204f8b70d9bc124e0d164a7d
srujana1233 <kakunurisrujana@gmail.com>
2021-04-23 08:06:28 +0530

    updated script.js

--- script.js
+++ script.js
@@ -1,30 +1,70 @@
 'use script'
 
-const secretNumber = Math.trunc(Math.radom() * 20) + 1;
+var secretNumber = Math.trunc(Math.radom() * 20) + 1;
 const number = document.querySelector('.number');
 
 const btnCheck = document.querySelector('.check');
+const btnAgain = document.querySelector('.again');
 const message = document.querySelector('.message');
 
 
 
 number.textContent = secretNumber;
 var score = 20;
-var displayScore = document.querySelector('.score');
+const displayScore = document.querySelector('.score');
 displayScore.textContent = score;
+
+var highScore = 0;
+const displayHighScore = document.querySelector('.highscore');
+displayHighScore.textContent = highScore;
 
 btnCheck.addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
-    if (guess > 0) {
-        if (guess = secretNumber) {
-            message.textcontext = 'your number is correct';
-        } else if (guess > secretNumber) {
-            message.textcontext = 'your number is high';
-        } else if (guess < secretNumber) {
-            message.textcontext = 'your number is low';
+    if (score > 0) {
+        if (guess > 0 && guess <= 20) {
+            if (guess = secretNumber) {
+                message.textcontext = 'your number is correct';
+                document.body.style.backgroundColor = 'green';
+                number.textContent = secretNumber;
+                number.style.width = '30rem';
+                if (score > highScore) {
+                    highScore = score;
+                    displayHighScore.textContent = highScore;
+                }
+
+            } else if (guess > secretNumber) {
+                message.textcontext = 'your number is high';
+                score--;
+                displayHighScore.textContent = score;
+            } else if (guess < secretNumber) {
+                message.textcontext = 'your number is low';
+                score--;
+                displayHighScore.textContent = score;
+            }
+        } else {
+            message.textcontext = 'Provide Right Input';
         }
     } else {
-        message.textcontext = 'Provide Right Input';
+        message.textContent = 'You lost the game';
+        document.body.style.backgroundColor = 'red';
+        number.textContent = secretNumber;
+        number.style.width = '30rem';
+
+        secretNumber = Math.trunc(Math.radom() * 20) + 1;
+        number.textContent = secretNumber;
+        score = 20;
+        displayScore.textContent = score;
+        message.textContent = 'Start Guessing';
+        document.querySelector('.guess').value = '';
     }
 
 });
+
+btnAgain.addEventListener('click', function() {
+            secretNumber = Math.trunc(Math.radom() * 20) + 1;
+            number.textContent = secretNumber;
+            score = 20;
+            displayScore.textContent = score;
+            message.textContent = 'Start Guessing';
+            document.querySelector('.guess').value = '';
+        }
